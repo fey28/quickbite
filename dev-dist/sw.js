49dca1275901d4bc1c9cd02abeb571459070022e
Aldq1 <feraru.alex1205@gmail.com>
2025-05-21 16:21:30 +0300

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ln8cjdbi5ng"
+    "revision": "0.24qopmeu3ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
